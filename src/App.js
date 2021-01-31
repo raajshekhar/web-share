9b7993f103c28d5a1f01e61c7053b2d2121f0c38
VRajashekhar <rvankayala@1800flowers.com>
2021-01-31 20:49:56 +0530

    Changed image

--- src/App.js
+++ src/App.js
@@ -2,21 +2,36 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const webShare = () => {
+    if (navigator.share) {
+      navigator.share({
+        title: 'Latest Brands',
+        text: "Keep the love growing valentine's gifts for them & you.",
+        url: 'https://www.plants.com/',
+      })
+        .then(() => {
+              console.log('Successful share')
+        })
+        .catch((error) => {
+          console.log('Error sharing', error)
+        });
+    } else {
+      console.log("Sorry, it's not working")
+    }
+  
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          We Can Share Information
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button id="share-option" onClick={webShare}>
+          Share
+        </button>
       </header>
     </div>
   );
